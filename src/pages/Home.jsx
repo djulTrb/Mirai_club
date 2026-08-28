1cbc81c812d133b6a09b4be89328d9151539d960
djulTrb <djitrb100@gmail.com>
2026-08-28 11:52:35 +0100

    fix: import useRef and useEffect in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import heroImage from '../assets/hero_illustration.png';
